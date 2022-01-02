e62fa6ab9d2a575a9ae4f96e82ce2dce604b6447
crocket77 <ethanpfeiffer345@outlook.com>
2022-01-02 12:19:05 -0600

    Window prompts and console logs

---
+++ assets/js/game.js
@@ -0,0 +1,10 @@
+var playerName = window.prompt("What is your robot's name?");
+console.log(playerName);
+console.log("this logs a string, good to leave a message")
+console.log(10+10);
+console.log("our robots name is " + playerName)
+function fight(){
+    window.alert("The fight has begun!")
+}
+
+//fight();
